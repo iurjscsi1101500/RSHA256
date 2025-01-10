7fbbb30fc2dbfbdaaadf56c000cb9a7b38802465
Atharva Tiwari <80307553+iurjscsi1101500@users.noreply.github.com>
2025-01-10 12:25:03 +0530

    display string before hashing it

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use rsha256::{sha256, print_hash};
 
 fn main() {
 	let exp_string = b"ABC";
+	println!("{}", std::str::from_utf8(exp_string).unwrap());
 	let hash = sha256(exp_string);
 	print_hash(&hash);
 	return;
